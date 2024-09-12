df94a2b5b627f1b8716c1cb693333090c1f9a84e
hassan <hassanqureshi9496@gmail.com>
2024-09-12 22:05:13 +0500

    linear search three

--- DSA/linearSearch.py
+++ DSA/linearSearch.py
@@ -1,26 +1,43 @@
 # Showing Message if elment is find
 
-def linear_search(arr , item):
-    for i in range(len(arr)):
-        if item == arr[i]:
-         return f"The number is found is index : {i} "
-    return f"The number {item} is not found in the list"
+# def linear_search(arr , item):
+#     for i in range(len(arr)):
+#         if item == arr[i]:
+#          return f"The number is found is index : {i} "
+#     return f"The number {item} is not found in the list"
 
-arr =['1' , '2' , '3' , '4' , '5']
-item='10'
+# arr =['1' , '2' , '3' , '4' , '5']
+# item='10'
 
 
-print(linear_search(arr , item))
+# print(linear_search(arr , item))
 
 
 #return that searched element and place the searched in zero index method 1
 
+# def linear_search(arr , item):
+#     for i in range(len(arr)):
+#         if arr[i] == item:
+#             temp = arr[0]
+#             arr[0] = arr[i]
+#             arr[i] = temp
+#             return arr
+#     return -1 
+        
+
+# arr =['1' , '2' , '3' , '4' , '5']
+# item='5'
+
+# result = linear_search(arr , item)
+# print(result)
+
+
+#return that searched element and place the searched in zero index method 2
+
 def linear_search(arr , item):
     for i in range(len(arr)):
         if arr[i] == item:
-            temp = arr[0]
-            arr[0] = arr[i]
-            arr[i] = temp
+            arr[0] , arr[i] = arr[i] , arr[0]
             return arr
     return -1 
         
@@ -30,6 +47,7 @@ item='5'
 
 result = linear_search(arr , item)
 print(result)
+
 
 
 
