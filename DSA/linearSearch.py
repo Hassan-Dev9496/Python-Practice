65341cc6de5941bea9ec94b6fcefffa6e2400f70
hassan <hassanqureshi9496@gmail.com>
2024-09-12 22:04:29 +0500

    linear search two

--- DSA/linearSearch.py
+++ DSA/linearSearch.py
@@ -13,6 +13,27 @@ item='10'
 print(linear_search(arr , item))
 
 
+#return that searched element and place the searched in zero index method 1
+
+def linear_search(arr , item):
+    for i in range(len(arr)):
+        if arr[i] == item:
+            temp = arr[0]
+            arr[0] = arr[i]
+            arr[i] = temp
+            return arr
+    return -1 
+        
+
+arr =['1' , '2' , '3' , '4' , '5']
+item='5'
+
+result = linear_search(arr , item)
+print(result)
+
+
+
+
 
 
 
