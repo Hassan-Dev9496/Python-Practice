3ee8e5593175fb45f8f3226fba958f22867c3fc3
hassan <hassanqureshi9496@gmail.com>
2024-09-15 23:01:25 +0500

    in method

--- DSA/linearSearch.py
+++ DSA/linearSearch.py
@@ -34,19 +34,33 @@
 
 #return that searched element and place the searched in zero index method 2
 
-def linear_search(arr , item):
-    for i in range(len(arr)):
-        if arr[i] == item:
-            arr[0] , arr[i] = arr[i] , arr[0]
-            return arr
-    return -1 
+# def linear_search(arr , item):
+#     for i in range(len(arr)):
+#         if arr[i] == item:
+#             arr[0] , arr[i] = arr[i] , arr[0]
+#             return arr
+#     return -1 
         
 
-arr =['1' , '2' , '3' , '4' , '5']
-item='5'
+# arr =['1' , '2' , '3' , '4' , '5']
+# item='5'
 
-result = linear_search(arr , item)
-print(result)
+# result = linear_search(arr , item)
+# print(result)
+
+#linear search in method
+
+
+def liner_search(arr , item):
+    if item in arr:
+        print("Yes item is in array")
+    else:
+        print("No")
+
+arr =['1' , '2' , '3' , '4' , '5']
+item='3'
+
+liner_search(arr , item)
 
 
 
