e5035daa805b6d1c1393b65a40e9bb2d9863b923
hassan <hassanqureshi9496@gmail.com>
2024-09-25 16:24:47 +0500

    sorting and bineary search combine

--- DSA/binarySearch.py
+++ DSA/binarySearch.py
@@ -15,3 +15,36 @@ target="40"
 print(binary_search(arr ,target ))
 
 
+#funtion which sorts the array first using selection sort and then find the target 
+
+def binearySearch(array , target):
+    left , right = 0 , len(arr) - 1
+    while left <= right:
+        mid =(left + right) // 2
+        if array[mid] == target:
+            return target
+        elif array[mid] > target:
+            right = mid-1
+        else:
+            array[mid] < target
+            left = mid+1
+
+def selection_sorting(array):
+    n=len(array)
+    for i in range(n-1):
+        least = i
+        for j in range(i+1 , n):
+            if array[j] < array[least]:
+                least = j
+        array[i] , array[least] = array[least] , array[i]
+    return array
+
+
+arr = [15 , 30 , 10 , 8 , 20 , 1]
+target = 8
+sorted_array = selection_sorting(arr)
+result = binearySearch(sorted_array , target)
+print(result , "result")
+
+
+
--- DSA/selectionSort.py
+++ DSA/selectionSort.py
@@ -1,4 +1,4 @@
-#sorting in ascending order
+# sorting in ascending order
 def selection_sorting(array):
     n=len(array)
     for i in range(n-1):
@@ -12,4 +12,31 @@ def selection_sorting(array):
 arr = [15 , 30 , 10 , 8 , 20 , 1]
 print(selection_sorting(arr))
 
+
+#sorting in descending_order
+
+def selection_sorting(array):
+    n = len(array)
+    for i in range(n-1):
+        highest=i
+        for j in range(i+1 , n):
+            if array[j] > array[highest]:
+                highest = j
+        array[i] , array[highest] = array[highest] , array[i]
+    return array
+
+arr = [15 , 30 , 10 , 8 , 20 , 1]
+print(selection_sorting(arr))
+
+
+
+
+
+
+
+
+
+
+        
+                
 
