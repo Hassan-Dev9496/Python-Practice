fcabf7c0c75a0ad80f73d25f8d5b1837102d339f
hassan <hassanqureshi9496@gmail.com>
2024-09-29 19:47:35 +0500

    bubble-sort on alphabets and removing duplicates

--- DSA/bubbleSort.py
+++ DSA/bubbleSort.py
@@ -13,3 +13,31 @@ def bubble_sorting(array):
 
 array = [1,2,3,4,5]
 print(bubble_sorting(array))
+
+#Sorting Alphabets and removing duplicate
+
+def dubplicate_checker(list_of_name):
+    visited=[]
+    for i in range(len(list_of_name)):
+        if list_of_name[i] not in visited:
+            visited.append(list_of_name[i])
+    return visited 
+
+def bubble_sorting_alphabets(name):
+    list_of_name= list(name)
+    size = len(list_of_name)
+    visited = set()
+    for i in range(size):
+        swapped = False
+        for j in range(0, size-i-1):
+            visited.add(list_of_name[j])
+            if list_of_name[j] > list_of_name[j+1]:
+                list_of_name[j] , list_of_name[j+1] = list_of_name[j+1] , list_of_name[j]
+                swapped = True
+        if not swapped:
+            break
+    return dubplicate_checker(list_of_name)
+
+
+name= "hassan"
+print(bubble_sorting_alphabets(name))
